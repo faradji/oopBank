0f4fe7b52fa0a74e1746fc10cdcbc9b0acdff13f
conta <conta@10.11.12.232>
2016-10-26 10:21:08 +0200

    Skapat scenerna och kodat lite

--- src/oopbank/FXMLCustomerInfoController.java
+++ src/oopbank/FXMLCustomerInfoController.java
@@ -4,19 +4,33 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class FXMLCustomerInfoController{
+    @FXML
+    Button btnAccountInformation;
     
     @FXML
-    public void clickedAddAccount() throws IOException{
+    private void clickedAddAccount() throws IOException{
         Stage accountStage = new Stage();
         Scene accountScene = new Scene(FXMLLoader.load(getClass()
                 .getResource("FXMLAddAccount.fxml")));
        
         accountStage.setScene(accountScene);
-        accountStage.show();
+        accountStage.initModality(Modality.APPLICATION_MODAL);
+        accountStage.show();       
+    }
+    
+    @FXML
+    private void clickedAccountInformation() throws IOException{
+        Stage accountInformationStage = 
+                (Stage)btnAccountInformation.getScene().getWindow();    
+        Scene accountInformationScene = new Scene(FXMLLoader.load(getClass()
+                .getResource("FXMLAccountInfo.fxml")));
         
+        accountInformationStage.setScene(accountInformationScene);                
     }
     
     @FXML
