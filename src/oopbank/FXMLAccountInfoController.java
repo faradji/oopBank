3dd37d725b6c73fb5896b03f1c177e9ff5728866
Henrik Rosqvist <Henrik Rosqvist@10.11.12.130>
2016-10-31 14:09:44 +0100

    Uppdaterat grafiska skärmar, koden för att gå mellan dem är inte komplett, men kommenterad

--- src/oopbank/FXMLAccountInfoController.java
+++ src/oopbank/FXMLAccountInfoController.java
@@ -7,7 +7,10 @@ package oopbank;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 /**
  * FXML Controller class
@@ -16,12 +19,33 @@ import javafx.fxml.Initializable;
  */
 public class FXMLAccountInfoController implements Initializable {
 
+    @FXML
+    private Label lblAccountType, lblBalance, lblCredit;
+    
+    @FXML
+    private ListView lvTransactions;
+    
+    @FXML
+    public void btnNewTransactionClicked()
+    {
+        //Ny transaktion, fönstret för detta är inte skapat än
+    }
+    
+    @FXML
+    public void btnBackClicked()
+    {
+        //Gå tillbaka till FXMLCustomerInfo
+    }
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // TODO 
+        //lblAccountType ska visa om det är credit eller savings
+        //lblBalance visar saldot
+        //lblCredit visar krediten, om det är ett credit konto
+        //lvTransactions visar en lista på transaktioner
     }    
     
 }
